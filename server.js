56c7ee8baa518d0cefb34a11c89c628b182bfc87
Kevin Guzman <kguzman@bdgsa.net>
2026-03-02 14:42:23 -0600

    uploading firts changes

--- server.js
+++ server.js
@@ -2,10 +2,13 @@
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
+
+
 /* ***********************
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
@@ -13,7 +16,15 @@ const static = require("./routes/static")
 /* ***********************
  * Routes
  *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") 
 app.use(static)
+
+//Index Route
+app.get("/", function(req, res){
+  res.render("index", {title:"Home"})
+})
 
 /* ***********************
  * Local Server Information
